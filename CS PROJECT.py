71616336102de6b88bcef8f4c3a62cb3ff1fd105
Akshay Bhandari <akshay.b1@outlook.com>
2022-01-06 10:15:21 +0530

    added skeleton

--- CS PROJECT.py
+++ CS PROJECT.py
@@ -1,5 +1,33 @@
 import mysql.connector
-sqlobj = mysql.connector.connect(host="localhost",password="1234",user="root",database="test")
+sqlobj = mysql.connector.connect(
+    host="localhost", password="1234", user="root", database="test")
 cursor = sqlobj.cursor()
-cursor.execute("create table abc (name char(20) , username char(20) , itemwon char(20),rupees_donated int(10)")
-con.commit
+# cursor.execute("create table abc (name char(20) , username char(20) , itemwon char(20),rupees_donated int(10)")
+# con.commit()
+
+bidItems = {}
+users = {}
+
+
+def registration():
+    pass
+
+
+def login():
+    pass
+
+
+def mainMenu():
+    pass
+
+
+def displayItems():
+    pass
+
+
+def bidForItem():
+    pass
+
+
+def addItemForBid():
+    pass
