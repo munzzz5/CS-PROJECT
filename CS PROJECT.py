f4071f42388f302b1cca477407e1345b81c26f42
Akshay Bhandari <akshay.b1@outlook.com>
2022-01-06 10:25:36 +0530

    added descriptions

--- CS PROJECT.py
+++ CS PROJECT.py
@@ -10,24 +10,30 @@ users = {}
 
 
 def registration():
+    # Take user input for name, username, password
     pass
 
 
 def login():
+    # Take user input for username and password and check from dictionary if username available and password matches
     pass
 
 
 def mainMenu():
+    # write menu options after login as 1. Add item 2. Bid for item 3. Display items 4. Logout
     pass
 
 
 def displayItems():
+    # write a loop to display all items of dictionary
     pass
 
 
-def bidForItem():
+def bidForItem(user, item, bidprice):
+    # check if current price is lower than bid price of item
     pass
 
 
 def addItemForBid():
+    # take user input for item name, price
     pass
